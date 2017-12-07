831b58166050f45035b2942281afe14190cafc04
MargauxVerdoy <mverdoy@hotmail.fr>
2017-12-07 11:15:40 +0100

    margaux version

---
+++ src/js/main.js
@@ -0,0 +1,20 @@
+//
+//   $("body").keydown(function(event){
+//     if ( event.which == 32 ) {
+//      event.preventDefault();
+//      $("video").attr("src", "medias/video2.mp4");
+//     }
+// });
+//
+// $("body").keyup(fonction(event){
+//
+// });
+//
+//
+// Code du prof
+$("body").keydown(function(e){
+ 32 == e.keyCode && ($(".premium").addClass("hide"), $("img").addClass("hide"));
+ });
+ $("body").keyup(function(e){
+  32 == e.keyCode && ($(".premium").removeClass("hide"), $("img").removeClass("hide"));
+  });
