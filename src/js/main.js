f660e479046b6fb2776d7631c25f254b2c1d36be
Olivier Céréssia <hello@olivier-ceressia.be>
2017-12-07 11:05:44 +0100

    no message

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 $("body").keydown(function(e) {
     32 == e.keyCode && ($("video:nth-of-type(1)").addClass("hide"), $("img").addClass("hidden"));
-}), $("body").keyup(function(e) {
+});
+$("body").keyup(function(e) {
     32 == e.keyCode && ($("video:nth-of-type(1)").removeClass("hide"), $("img").removeClass("hidden"));
 });
